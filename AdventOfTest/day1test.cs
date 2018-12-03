0c47bb5373a341e10ea9f3196049d554e6b2cb72
therealjawss <jawss@outlook.com>
2018-12-03 13:14:42 +0100

    InitialCommit

---
+++ AdventOfTest/day1test.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfTest
+{
+    [TestClass]
+    public class day1test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ day1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = readSequence("input.txt");
+
+            int sum = 0;
+            int i = 0;
+            bool frequencyRepeated = false;
+            List<int> frequencies = new List<int>();
+            while (!frequencyRepeated) {
+                sum += Int32.Parse(lines[i]);
+                if (frequencies.Contains(sum))
+                {
+                    frequencyRepeated = true;
+                }
+                else {
+                    frequencies.Add(sum);
+                }
+                if (++i == lines.Length) {
+                    i = 0;
+                }
+            }
+            
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+
+        public static string[] readSequence(string filename) {
+            string[] lines = System.IO.File.ReadAllLines(@".\"+filename);
+            return lines;
+        }
+    }
+}
